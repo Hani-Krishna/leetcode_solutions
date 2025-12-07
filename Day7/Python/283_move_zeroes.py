e8b3590475d032b410bd2be2aabb80193f7a3660
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-07 20:40:43 +0530

    Implement moveZeroes function to shift zeros

---
+++ Day7/Python/283_move_zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        i = 0  
+        for n in nums:
+            if n != 0:
+                nums[i] = n
+                i += 1
+        
+        while i < len(nums):
+            nums[i] = 0
+            i += 1
