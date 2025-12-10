8d4efcc750a7ecbcad474886ea593aa75b5b5bad
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-10 23:47:48 +0530

    Create 1158_market_analysis_I.sql

---
+++ Day10/SQL/1158_market_analysis_I.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+U.USER_ID AS buyer_id,     
+U.JOIN_DATE AS join_date,
+SUM(
+IF(YEAR(ORDER_DATE) = 2019, 1, 0)) AS orders_in_2019    
+FROM USERS U
+LEFT JOIN ORDERS O
+ON U.USER_ID = O.BUYER_ID 
+GROUP BY 
+U.USER_ID, 
+U.JOIN_DATE;
