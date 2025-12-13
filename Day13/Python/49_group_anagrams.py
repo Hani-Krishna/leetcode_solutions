1658c6d811ccbec9bbf0452e39fe1d6cd4bc4fd9
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-13 21:33:52 +0530

    Create 49_group_anagrams.py

---
+++ Day13/Python/49_group_anagrams.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        from collections import defaultdict
+        mp = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            mp[key].append(word)
+
+        return list(mp.values())
