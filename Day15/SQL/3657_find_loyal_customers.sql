0206ad98b59fc2ec7ac9700fc05463bcf3163f2a
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-16 01:56:01 +0530

    Create 3657_find_loyal_customers.sql

---
+++ Day15/SQL/3657_find_loyal_customers.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT
+    customer_id
+FROM customer_transactions
+GROUP BY customer_id
+HAVING
+    -- At least 3 purchases
+    SUM(CASE WHEN transaction_type = 'purchase' THEN 1 ELSE 0 END) >= 3
+
+    -- Active for at least 30 days
+    AND DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30
+
+    -- Refund rate < 20%
+    AND (
+        SUM(CASE WHEN transaction_type = 'refund' THEN 1 ELSE 0 END) * 1.0
+        / COUNT(*)
+    ) < 0.2
+ORDER BY customer_id;
