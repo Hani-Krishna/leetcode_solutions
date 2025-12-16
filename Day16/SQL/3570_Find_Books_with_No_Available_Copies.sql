b3272af20a5dd5ee13f52169041beac8c92b49ab
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-17 00:44:19 +0530

    Create 3570_Find_Books_with_No_Available_Copies.sql

---
+++ Day16/SQL/3570_Find_Books_with_No_Available_Copies.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+SELECT
+b.book_id,
+b.title,
+b.author,
+b.genre,
+b.publication_year,
+COUNT(br.record_id) AS current_borrowers
+FROM library_books b
+JOIN borrowing_records br
+ON b.book_id = br.book_id
+AND br.return_date IS NULL
+GROUP BY
+b.book_id,
+b.title,
+b.author,
+b.genre,
+b.publication_year,
+b.total_copies
+HAVING COUNT(br.record_id) = b.total_copies
+ORDER BY
+current_borrowers DESC,
+b.title ASC;
