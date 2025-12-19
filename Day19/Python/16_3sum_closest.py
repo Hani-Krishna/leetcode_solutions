049761ea01aabfd78bb0e425ed1aae528cf792d9
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-19 08:55:39 +0530

    Create 16_3sum_closest.py

---
+++ Day19/Python/16_3sum_closest.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        n = len(nums)
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                # update closest if current is nearer
+                if abs(curr_sum - target) < abs(closest - target):
+                    closest = curr_sum
+
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+                else:
+                    return target   # exact match
+
+        return closest
+
+        
