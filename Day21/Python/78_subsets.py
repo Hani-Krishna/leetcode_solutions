9bbbf3355534c966082c2559d4cd369301c66145
Hani Krishna <164912541+Hani-Krishna@users.noreply.github.com>
2025-12-21 09:38:27 +0530

    Create 78_subsets.py

---
+++ Day21/Python/78_subsets.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def subsets(self, nums):
+        result = [[]]
+        
+        for num in nums:
+            result += [curr + [num] for curr in result]
+        
+        return result
+
+    
+
+
+
+ 
+
+  
+  
+
+            
+
+        
